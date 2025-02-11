e9be2c57c8e3e65d483076d30bdb55f6d94d9eea
dawitzeleke <dawitzeleke92@gmail.com>
2025-02-11 18:05:58 +0300

    setup clean architecture

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,6 +1,8 @@
+using MongoDB.Driver;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
