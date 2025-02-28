fab23033bf233a7fecb001f26d6b4a6e3ee93960
Betelhem <betelhemyimamd@gmail.com>
2025-02-28 11:17:25 +0300

    add contents common definition

---
+++ backend/Domian/Common/ContentEntity.cs
@@ -0,0 +1,11 @@
+using backend.Domain.Common;
+using backend.Domain.Enums;
+
+namespace backend.Domain.Common;
+
+public abstract class ContentEntity: BaseEntity
+{
+    public ContentTypeEnum type { get; set; }
+    public string CreatedBy { get; set; }
+
+}
