14c7af59e5913b749cfff4140c77578c466bebbe
Betelhem <betelhemyimamd@gmail.com>
2025-02-28 11:15:46 +0300

    add question feature, create command and handler

---
+++ backend/Application/Features/Questions/Commands/CreateQuestion/CreateQuestionCommand.cs
@@ -0,0 +1,19 @@
+using MediatR;
+using backend.Domain.Enums; 
+
+namespace backend.Application.Features.Questions.Commands.CreateQuestion;
+
+public class CreateQuestionCommand: IRequest<bool>
+{
+    public string QuestionText { get; set; }
+    public string Description { get; set; }
+    public string[] Options { get; set; }
+    public string CorrectOption { get; set; }
+    public string Answer { get; set; }
+    public string CourseName { get; set; }
+    public int Points { get; set; }
+    public int Grade { get; set; }
+    public DifficultyLevel Difficulty { get; set; }
+    public QuestionTypeEnum QuestionType { get; set; }
+    public string CreatedBy { get; set; }
+}
