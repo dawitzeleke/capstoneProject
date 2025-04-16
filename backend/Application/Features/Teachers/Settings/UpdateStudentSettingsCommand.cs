d0082b0db7b73faf376a0f4494cc25abe35086eb
dawitzeleke <dawitzeleke92@gmail.com>
2025-04-16 14:03:56 +0300

    start the teacher settings feature

---
+++ backend/Application/Features/Teachers/Settings/UpdateStudentSettingsCommand.cs
@@ -0,0 +1,11 @@
+using MediatR;
+
+public class UpdateTeacherSettingsCommand :  IRequest<bool>
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string UserName { get; set; }
+    public string ProfilePictureUrl { get; set; }
+}   
