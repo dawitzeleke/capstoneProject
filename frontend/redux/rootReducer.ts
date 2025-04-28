bcb13bb179b2271aee4a0b8845da2f956a2b2f54
Meti Guda <silvie8202@gmail.com>
2025-04-28 16:12:49 +0300

    feat(teacher/redux): integrate content reducer into root store
    - Import contentReducer from contentSlice
    - Add to combined reducers under 'content' key
    - Update RootState type definition automatically
    
    Enables access to content management state throughout the application via Redux

--- frontend/redux/rootReducer.ts
+++ frontend/redux/rootReducer.ts
@@ -3,12 +3,14 @@ import optionReducer from "./optionReducer/optionReducer";
 import questionsReducer from "./questionsReducer/questionsReducer";
 import savedQuestionsReducer from "./savedQuestionsReducer/savedQuestionReducer";
 import teacherReducer from "./teacherReducer/teacherReducer";
+import contentReducer from "./teacherReducer/contentSlice";
 
 const rootReducer = combineReducers({
   option: optionReducer,
   questions: questionsReducer,
   teacher: teacherReducer,
   savedQuestions:savedQuestionsReducer,
+  content: contentReducer,
 });
 
 export default rootReducer;
