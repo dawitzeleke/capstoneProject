2b060aa8f66d84385f43123649e34bc12645c660
Meti Guda <silvie8202@gmail.com>
2025-05-22 14:33:24 +0300

    fix(teacher/redux): update import paths for teacher reducers to use slice files

--- frontend/redux/rootReducer.ts
+++ frontend/redux/rootReducer.ts
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import optionReducer from "./optionReducer/optionReducer";
 import questionsReducer from "./questionsReducer/questionsReducer";
 import savedQuestionsReducer from "./savedQuestionsReducer/savedQuestionReducer";
-import teacherReducer from "@/redux/teacherReducer/teacherReducer";
+import teacherReducer from "@/redux/teacherReducer/teacherSlice";
 import mediaReducer from "@/redux/teacherReducer/mediaSlice";
 import notificationsReducer from "@/redux/teacherReducer/notificationsSlice"
 import teacherQuestionReducer from "@/redux/teacherReducer/teacherQuestionSlice"
