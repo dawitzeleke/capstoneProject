57bd84b267cd698d435aabfa6c698ba97b3797ce
Betelhem <betelhemyimamd@gmail.com>
2025-05-26 18:38:46 +0300

    Add missing  namespace

--- backend/webApi/Controllers/ReportController.cs
+++ backend/webApi/Controllers/ReportController.cs
@@ -1,4 +1,5 @@
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using backend.Application.Features.ReportFeature.Commands.CreateReport;
